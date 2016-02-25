5e00e056bfaac71b3ed598612570837a28607214
Boaz Blake <boazblake@gmail.com>
2016-02-24 23:43:13 -0600

    profile data complete - added email- -pivoted website to be a hub for TIY alumni - captured primary data from repos- need to style (both)

--- js/script.js
+++ js/script.js
@@ -1,48 +1,105 @@
 console.log($)
 
 // var apiKey = ''
-// repos=========='https://api.github.com/users/boazblake/repos'
-var baseURL= 'https://api.github.com/users/boazblake'
+var profileURL= 'https://api.github.com/users/boazblake'
+var reposURL= 'https://api.github.com/users/boazblake/repos'
 
-var fullURL = baseURL 
+// var websiteURL  = [profileURL, reposURL]
 
-var promise = $.getJSON(fullURL)
+var promiseProfile = $.getJSON(profileURL)
+var promiseRepos = $.getJSON(reposURL)
 
+//Profile Data Function
 
-var handleData = function(jsonData) {
-	// console.log(jsonData)
+var handleDataProfile = function(jsonProfileData) {
+	console.log(jsonProfileData)
 	var domString = ''
-	var profileObject = jsonData
-	// console.log(profileObject)
+	var profileObject = jsonProfileData
 	domString += profileToHTML(profileObject)
-
 
 var containerEl = document.querySelector('.left')
 	containerEl.innerHTML = domString
-
 }
 
+//Profile Repos Function
+
+var handleDataRepos = function(jasonDataRepo) {
+	var gitURLArray = []
+	var repoNameArray = []
+	var issuesCountArray= []
+	for (var i = 0; i < jasonDataRepo.length; i++) {
+		gitURLArray[i] = jasonDataRepo[i].full_name
+		repoNameArray[i] = jasonDataRepo[i].git_url
+		issuesCountArray[i] = jasonDataRepo[i].open_issues_count
+
+	}
+	// console.log(gitURLArray)
+	// console.log(repoNameArray)
+	// console.log(issuesCountArray)
+
+
+	
+
+
+
+
+
+
+
+
+
+
+	
+	// domString += repoToHTML(repoArray)
+
+// var containerEl = document.querySelector('.right')
+	// containerEl.innerHTML = domString
+}
+
+
+
+//Profile Data to DOM
+
 var profileToHTML = function (profileObject) {
-	console.log(profileObject)
+	// console.log(profileObject)
 	var avatarImgSrc = profileObject.avatar_url
-	// console.log(avatarImgSrc)
 	var name = profileObject.name
-	// console.log(name)
 	var email = profileObject.email 
 	var blog = profileObject.blog
 	var hire = profileObject.hireable 
 	var bio = profileObject.bio
+	var location = profileObject.location
 
 	var newProfileToDom ='<div class="imgContainer"><img src="' + avatarImgSrc + '"></div>'
 	 newProfileToDom +='<ul class="profileListContainer"><li class="profileName">' + name + '</li>'
 	 newProfileToDom += '<li class="profileBlog">' + blog + '</li>'
 	 newProfileToDom += '<li class="profileHireable">' + hire + '</li>'
 	 newProfileToDom += '<li class="profileEmail">' + email + '</li>'
+	 newProfileToDom += '<li class="profileLocation">' + location + '</li>'
 	 newProfileToDom += '<li class="profileBio">' + bio + '</li></ul>'
 
 	return newProfileToDom
-	console.log(newProfileToDom)
 }
 
-promise.then(handleData)
+
+//Profile Repo To Dom
+
+
+
+
+
+
+
+
+
+
+promiseProfile.then(handleDataProfile)
+promiseRepos.then(handleDataRepos)
+
 console.log()
+
+
+
+
+
+
