18f7e6105a9eb3a539fc4290d0d27d2fdba4ffb7
Boaz Blake <boazblake@gmail.com>
2016-02-26 07:37:50 -0600

    updates

--- js/script.js
+++ js/script.js
@@ -1,37 +1,41 @@
 console.log($)
 
-var apiKey = '?access_token=4ec2ce71fa46864e2eba86d05dd76f62a9e88d7b'
+var apiKey = '?access_token=5925f368be9ca667bdf8b3d31f48ecfb46594a35'
 var profileURL = 'https://api.github.com/users/'
 var reposURL = 'https://api.github.com/users/'
 var userProfile = 'boazblake'
-var userRepo = 'boazblake/repos'
+var userRepo = userProfile + '/repos'
 
-var promiseProfile = $.getJSON(profileURL + userProfile)
-var promiseRepos = $.getJSON(reposURL + userRepo)
+var promiseProfile = $.getJSON(profileURL + userProfile + apiKey)
+var promiseRepos = $.getJSON(reposURL + userRepo + apiKey)
+var inputEl = document.querySelector("input")
+var userInput = inputEl.value
+
 
 //Profile Data Function
 
 var handleDataProfile = function(jsonProfileData) {
-    console.log(jsonProfileData)
+    // console.log(jsonProfileData)
     var domPRofileString = ''
     var profileObject = jsonProfileData
     domPRofileString += profileToHTML(profileObject)
     var profileContainer = document.querySelector('.left')
     profileContainer.innerHTML = domPRofileString
+
 }
 
-// Repos Data Function
+// Repos Data Function & sending to DOM
 
 var handleDataRepos = function(jasonDataRepo) {
-	// console.log(jasonDataRepo)
+	console.log([jasonDataRepo])
     var domRepoString = ''
     for (var i = 0; i < jasonDataRepo.length; i++) {
     	domRepoString += '<a href="jasonDataRepo[i].url"><div class="repoList"> <h4>Repo Name:     ' + jasonDataRepo[i].name + '</h2><br>Number Of Open Issues:     '+ jasonDataRepo[i].open_issues_count+'</div></a>'
-    	// console.log(domRepoString)
     } 
-    // domRepoHTML = repoToHTML(domRepoString)
     var repoContainer = document.querySelector('.right')
    	repoContainer.innerHTML = domRepoString
+    	console.log()
+
 }   
 
 //Profile Data to DOM
@@ -51,7 +55,7 @@ var profileToHTML = function(profileObject) {
 
     var newProfileToDom = '<img class="profilePic" src="' + avatarImgSrc + '">'
     newProfileToDom += '<ul class="profileListContainer"><li class="profileName"><h3>' + name + '</h3></li>'
-    newProfileToDom += '<li class="profileEmail">' + email + '</li>'
+    newProfileToDom += '<i class="fa fa-envelope"></i><li class="profileEmail">' + email + '</li>'
     newProfileToDom += '<li class="profilelocation">' + location + '</li></ul><br><br>'
     newProfileToDom += '<div class="profileBlog box">' + blog + '</div>'
     newProfileToDom += '<div class="profileBio box">' + bio + '</div>'
@@ -60,21 +64,48 @@ var profileToHTML = function(profileObject) {
     return newProfileToDom
 }
 
-// // search function
+// search function
 
-// var handleUserInput = function(keyEvent) {
-// 	if (event.keycode === 13) {
-// 		var searchInput = keyEvent.target
-// 		var textSearch = searchInput.value
-// 		searchInput.value = ''
-// 	}
-// 	var searchURL = 'https://api.github.com/search/'
-// 	var profileURL = searchURL + textSearch + apiKey
-// 	var userRepo = searchURL + textSearch + apiKey
-// 	console.log(profileURL)
-// 	console.log(userRepo)
+var newSearch = function(keyEvent) {
+		var inputEl = keyEvent.target
+	if (keyEvent.keycode === 13) {
+		var query = inputEl.value
+	console.log(inputEl.value)
+
+		inputEl.value = ''
+	}
+	var searchURL = 'https://api.github.com/search/'
+	var profileURL = searchURL + query + apiKey
+	var repoURL = searchURL + query + '/repos' + apiKey
+
+	var promiseProfile = $.getJSON(profileURL)
+var promiseRepos = $.getJSON(repoURL)
+	promiseProfile.then(handleDataProfile)
+promiseRepos.then(handleDataRepos)
+	console.log([handleDataProfileSearch])
+	console.log([handleDataReposSearch])
+}
+
+
+
+
+// var newSearch = function(keyEvent) {
+//   var targetEl = keyEvent.target
+//   // console.log(targetEl)
+
+//   if (keyEvent.keyCode === 13) {
+//       numberOfZip = inputEl.value
+//       zipper = parseInt(numberOfZip) + '&apikey='
+// 	  pathName = baseUrl + 'zip=' +zipper + apiKey
+// 	  var promise = $.getJSON(pathName)
+// 	  console.log(promise)
+// 	  promise.then(handleData)
+// 	  // function that refreshes the page with the pat
+
+//   }
 // }
 
+// inputEl.addEventListener('keydown',addItem)
 
 
 
@@ -84,6 +115,8 @@ var profileToHTML = function(profileObject) {
 
 
 
-// searchInput.addEventListener('keydown', handleUserInput)
+
+inputEl.addEventListener('keydown', newSearch)
+
 promiseProfile.then(handleDataProfile)
 promiseRepos.then(handleDataRepos)
