f722c3f73ae461a16db98db375d49f0c84884d99
boazblake <boazblake@gmail.com>
2016-04-17 11:44:26 -0500

    added Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This application explores API calling and how to overcome JS's asynchronization limitaions by using fetch and promises.
+
+This app is a proof of concept for a future idea for an internal network for the Iron Yard family / social network groups focused on assisiting each other directly through github. 
--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+
+// check to see if we are in contact with jQuery library
 console.log($)
 
-// var apiKey = '?access_token=0a0354a66ccbc353d830011d91f3f4556279ce07'
+// var apiKey = '?access_token=xxxx'
+
+// DEFINING OUR VARIABLES AND FUNCTIONS
+// the defualt loading page is the API base URL, with my username.
 var baseURL = 'https://api.github.com/users/'
 var userProfile = 'boazblake'
 var userRepo = userProfile + '/repos' 
+
+// the Search bar is located using ghr queryselector and saved as the variable 'inputEl'.
 var inputEl = document.querySelector("input")
 
+
+ /////////////////////////////////////////
+// The PROMISE function for jQuery 
+/////////////////////////////////////////
+// is defined and takes in 2 variabels the input URL and, if needed, a parameter object. If there is a parametre object (paramsObj === 'true') then the helper function defined below is invoked (ln.26----- we can define the function BELOW the calling of it in this case because the invocation of the function doesnt happen UNTIL after the entire JS script has been read and stored into the memory.) and stored in the variable 'formattedParams' which is then resolved alongside the inputURL as the complete URL to be used in the promise for fetching and resolving as the JSON data. (was stumped for a while on this one...)If no paramaters are found, then formattedParam returns as an empty string and gets passed along with no affect (and not as I had erroniasly thought as 'undefiend'...thanks dev tools)
 function makeAndReturnPromise(inputURL, paramsObj) {
     var formattedParams = ''
     if( paramsObj ) {formattedParams = _formatURLParams(paramsObj) }
@@ -13,7 +25,9 @@ function makeAndReturnPromise(inputURL, paramsObj) {
     return $.getJSON(inputURL + formattedParams )
 }
 
-var _formatURLparams = function(paramsObj) {
+
+// defining the parameter helper function.   '_formatURLparams' helper function comes into play if the  'paramsObj' === 'true' and resolves into the appropraite parameter string for the URL. This helper function is then used to complete the URL for 'makeAndReturnPromise' function (defined above) the helper function takes as the input the parsamter object and formats it into an HTML string including the '&' and '='
+var _formatURLParams = function(paramsObj) {
    var paramString = ''
    for (var aKey in paramsObj) {
        var val = paramsObj[aKey]
@@ -21,6 +35,11 @@ var _formatURLparams = function(paramsObj) {
    }
    return paramString.substr(1)
 }
+
+
+/////////////////////////////////////////
+// Defining the helper function for the controller that resolves into the URL 
+/////////////////////////////////////////
 
 function _gitUserURL(userName) {
 
@@ -35,6 +54,10 @@ function _gitUserRepoURL(userName){
 }
 
 
+
+
+
+/////////////////////////////////////////
 //Profile Data Function
 
 var handleDataProfile = function(jsonProfileData) {
@@ -46,6 +69,9 @@ var handleDataProfile = function(jsonProfileData) {
     profileContainer.innerHTML = domPRofileString
 }
 
+
+
+/////////////////////////////////////////
 //Profile Data to DOM
 
 var profileToHTML = function(profileObject) {
@@ -71,6 +97,8 @@ var profileToHTML = function(profileObject) {
     return newProfileToDom
 }
 
+
+/////////////////////////////////////////
 // Repos Data Function & sending to DOM
 
 var handleDataRepos = function(jasonDataRepo) {
@@ -85,18 +113,37 @@ var handleDataRepos = function(jasonDataRepo) {
 
 }
 
+
+
+//////////////////SEARCH FUNCTION/////////////////////////
+
+//////////////////////////////////////////////
+// STEP 1. DEFINING THE SEARCH FUNCTION///////////////
+// the function newSearch (ln.96) accepts the input 'keyEvent' for the 'enter' (keyEvent.keyCode number 13) key to be pressed down. At this point the contents of the search    ('.value') [the username at this point of web-biilding is as complex as this app can get]is stored as a variable 'userLookupVal' and the contents of the search are cleared. The the '.location.hash' attribute of the window is defined as the search input variable which proceeds to place a # mark and the value of the userLookupVal in the search bar. This affects the initial on-load (defined below) on a hard re-fresh of the window.
+//////////////////////////////////////////////
 var newSearch = function(keyEvent) {
     var inputEl = keyEvent.target
 
     if (keyEvent.keyCode === 13) {
         var userLookupVal = inputEl.value
         inputEl.value = ''
-        window.location.hash = userLookupVal
+        window.location.hash = userLookupVal //
 
 	}
 }
 
+////////////////////////////////////////////
+// STEP 2. CALLING SEARCH//////////////////
+// This statement calls the eventlistner method of the search input object(inputEl). The event listner is set to listen for a 'keydown' (defined above) and passes the contents to the variable 'newSearch' ( defined above as the function newSearch ). The inputEl.addEventListener method essentially calls the 'newSearch' function and passes along the built-in option of 'keydown'.
+
 inputEl.addEventListener('keydown', newSearch)
+
+
+
+
+////////////////////////////////////////////
+// THE CONTROLLER: //////////////////
+// The controller receives  input 'hashEvent' from thre ways: 1. when loading a new page, 2. the search value input (as defined above), 3. using a 'deep link' which affects the hashmark of the on-load (as defined below). Each of these inputs resolves into a # and username (either from the contents of the search function or via the deep link). The first thing the controller does is strip the hashtag and save the username as the variable 'userLookupVal' which is then used in the jQuery promise function which has the attribute '.then'. this function (defined above ln. 9) takes in as an input the API url for the profile and repo and the '.then' attribute sends the resultant JSON object containing our requested data to the variable 'handleData...'(defined above ln.40 & 51 for the profile or repo as the case warrents). 
 
 var controller = function (hashEvent) {
      // console.log(window.location.hash)
@@ -111,7 +158,7 @@ var controller = function (hashEvent) {
 }
  ///////////////////////////
 // webApp initialization.
-// The first thing that happens on loading page is initiatied by (117 in conjuncture with 126 --- Ty Travis H.) Due to the script having both the window.location.hash resolving as a boolean response of 'true' (117) AND the Window (126) 'hashchange'  evventlistner activated capturing any eventualaity and passing to the conroller (defined above). Otherwise, the helper functions which resolve into the user and repo URLs and subsequently defined into thier own variables (120-121) and then called  (122&123) by the jQuery promise-calling-method which involved the actual making and returning function (defined above) having the '.then' method (defined above l.40) resolving () applied which 
+// The first thing that happens on loading page is initiatied by (117 in conjuncture with 126 (AND affected by the search input ---post refresh of page (88-94)---)--- Ty Travis H.) Due to the script having both the window.location.hash resolving as a boolean response of 'true' (117) AND the Window (126) 'hashchange'  evventlistner activated to capture any eventualaity that is then passed to the conroller (defined above). Otherwise, the helper functions which resolve into the user and repo URLs and subsequently defined into thier own variables (120-121) and then called  (122&123) by the jQuery promise-calling-method which involved the actual making and returning function (defined above ln. 9 & 16) resolving into the required URLs and being passed along to the attached'.then' method which has an input the variable that resolves into a function that will handle the returned JSON data. (defined above l.40) 
 //////////////////////////////////
 // console.log(window.location.hash)
 if (window.location.hash) {
